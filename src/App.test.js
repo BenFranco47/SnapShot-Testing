29bed48871506938525e515f7d96df930b7c30dd
Ben Franco <88692753+BenFranco47@users.noreply.github.com>
2022-02-11 20:34:53 -0500

    Created Test and ran them, got a pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import renderer from 'react-test-renderer'
 
-test('renders a snapshot', () => {
+
+//import renderer from 'react-test-renderer'
+
+/*test('renders a snapshot', () => {
   const tree = renderer.create(<App/>).toJSON()
   expect(tree).toMatchSnapshot()
-})
+})*/
+
+test('Searching for Ben Franco text to render', () => {
+  render(<App />)
+  const nameElement = screen.getByText(/Ben Franco/i)
+  expect(nameElement).toBeInTheDocument()
+
+});
